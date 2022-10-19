c991d9a55be23ce467d3fcc5869e113565ae67c1
Francisco Pires <98482270+PireXa@users.noreply.github.com>
2022-10-19 16:09:50 +0100

    Add files via upload

--- inc/minishell.h
+++ inc/minishell.h
@@ -40,88 +40,80 @@
 
 typedef struct s_list
 {
-    char *content;
-    struct s_list *next;
-}               t_list;
+	char			*content;
+	struct s_list	*next;
+}			t_list;
 
 typedef struct s_exporttable
 {
-    char *key;
-    char *value;
-    struct s_exporttable *next;
-}               t_exporttable;
+	char					*key;
+	char					*value;
+	struct s_exporttable	*next;
+}				t_exporttable;
 
 typedef struct s_cmds
 {
-    char *cmd;
-    struct s_cmds *next;
-}               t_cmds;
+	char			*cmd;
+	struct s_cmds	*next;
+}			t_cmds;
 
 typedef struct s_lists
 {
-    t_exporttable **export;
-    char *line;
-    char ***cmds;
-}               t_minithings;
+	t_exporttable	**export;
+	char			*line;
+	char			***cmds;
+}				t_minithings;
 
 // Functions
 
-void sig_handler(void);
-void	sig_handler_block(void);
-void commands(t_minithings *minithings, char **envp);
-void builtins(t_minithings *minithings);
-int is_builtin(char *str);
-int is_not_pipeable(char *str);
-int ft_strncmp(const char *s1, const char *s2, size_t n);
-char *ft_strchr(const char *s, int c);
-char	*ft_strdup(const char *s1);
-char *ft_substr(char const *s, int start, size_t len);
-char	**ft_split(char const *s, char c);
-void   pipex(int nbr_cmds, char ***cmds, char **envp, t_minithings *minithings);
-char *ft_strtrim(char *str);
-int ft_strcmp(const char *s1, const char *s2);
-char **quote_splitter(char *line);
-
-void export(t_minithings *minithings);
-void show_export_list(t_minithings *minithings, int flag);
-t_exporttable *add_export_node(char *key, char *value);
-void add_export_node_front(t_exporttable **head, t_exporttable *new);
-void add_export_node_back(t_exporttable **lst, t_exporttable *new);
-t_exporttable 	*indxexport(t_exporttable *list, int index);
-int ft_lstsize(t_exporttable *lst);
-int     check_duplicated(t_exporttable **export, char *str);
-void    value_modifier(t_exporttable **export, char *value, int i);
-
-void unset(t_minithings *minithings);
-
-void    ft_sort_list(t_exporttable *tab, int size);
-t_cmds *ft_lstnew(void *content);
-void ft_lstadd_back(t_cmds **lst, t_cmds *new);
-void ft_lstadd_front(t_cmds **lst, t_cmds *new);
-t_list *indx(t_list *lst, int index);
-
-char	**ft_split(char const *s, char c);
-int	ft_strlen(const char *str);
-char	*ft_substr(char const *s,  int start, size_t len);
-char	*ft_strdup(const char *s1);
-char	*ft_strjoin(char *s1, char *s2);
-
-char	*ft_strchr(const char *s, int c);
-void child_one(char **av,  t_minithings *minithings, char **envp);
-void execute(char **cmds,  t_minithings *minithings, char **envp);
-
-int		ft_strncmp(const char *s1, const char *s2, size_t n);
-char	*ft_exstrchr(char *s);
-char	*ft_strnldup(char *s1);
-size_t	ft_strlcpy(char *dst, char *src, size_t size);
-
-void	*ft_calloc(size_t count, size_t size);
-char	*ft_strnstr(const char	*big, const char *little, size_t len);
-char	*find_path(char *cmd, t_exporttable **envp);
-char ***parser(char *line, t_exporttable **export);
-
-void free_triple_pointer(char ***triple);
-void    print_triple_pointer(char ***triple);
-void free_double_array(char **array);
+t_exporttable	*add_export_node(char *key, char *value);
+void			sig_handler(void);
+void			sig_handler_block(void);
+void			commands(t_minithings *minithings, char **envp);
+void			builtins(t_minithings *minithings);
+int				is_builtin(char *str);
+int				is_not_pipeable(char *str);
+int				ft_strncmp(const char *s1, const char *s2, size_t n);
+char			*ft_strchr(const char *s, int c);
+char			*ft_strdup(const char *s1);
+char			*ft_substr(char const *s, int start, size_t len);
+char			**ft_split(char const *s, char c);
+void			pipex(int nbr_cmds, char ***cmds, char **envp, t_minithings *minithings);
+char			*ft_strtrim(char *str);
+int				ft_strcmp(const char *s1, const char *s2);
+char			**quote_splitter(char *line);
+void			export(t_minithings *minithings);
+void			show_export_list(t_minithings *minithings, int flag);
+void			add_export_node_front(t_exporttable **head, t_exporttable *new);
+void			add_export_node_back(t_exporttable **lst, t_exporttable *new);
+t_exporttable	*indxexport(t_exporttable *list, int index);
+int				ft_lstsize(t_exporttable *lst);
+int				check_duplicated(t_exporttable **export, char *str);
+void			value_modifier(t_exporttable **export, char *value, int i);
+void			unset(t_minithings *minithings);
+void			ft_sort_list(t_exporttable *tab, int size);
+t_cmds			*ft_lstnew(void *content);
+void			ft_lstadd_back(t_cmds **lst, t_cmds *new);
+void			ft_lstadd_front(t_cmds **lst, t_cmds *new);
+t_list			*indx(t_list *lst, int index);
+char			**ft_split(char const *s, char c);
+int				ft_strlen(const char *str);
+char			*ft_substr(char const *s, int start, size_t len);
+char			*ft_strdup(const char *s1);
+char			*ft_strjoin(char *s1, char *s2);
+char			*ft_strchr(const char *s, int c);
+void			child_one(char **av, t_minithings *minithings, char **envp);
+void			execute(char **cmds, t_minithings *minithings, char **envp);
+int				ft_strncmp(const char *s1, const char *s2, size_t n);
+char			*ft_exstrchr(char *s);
+char			*ft_strnldup(char *s1);
+size_t			ft_strlcpy(char *dst, char *src, size_t size);
+void			*ft_calloc(size_t count, size_t size);
+char			*ft_strnstr(const char	*big, const char *little, size_t len);
+char			*find_path(char *cmd, t_exporttable **envp);
+char			***parser(char *line, t_exporttable **export);
+void			free_triple_pointer(char ***triple);
+void			print_triple_pointer(char ***triple);
+void			free_double_array(char **array);
 
 #endif
--- srcs/pipex/pipex_utils.c
+++ srcs/pipex/pipex_utils.c
@@ -12,71 +12,20 @@
 
 #include "../../inc/minishell.h"
 
-char	*ft_str_cmdpath(const char *s1)
+int	ft_strncmp(const char *s1, const char *s2, size_t n)
 {
-	char	*str;
 	size_t	i;
-	size_t	a;
 
-	str = (char *)malloc(sizeof(*s1) * (ft_strlen(s1) + 2));
-	if (!str)
-		return (NULL);
-	str[0] = '/';
-	i = 1;
-	a = -1;
-	while (s1[++a] && s1[a] != ' ')
-		str[i++] = s1[a];
-	str[i] = 0;
-	return (str);
-}
-
-int	ft_access(char	*tmp, char	**paths, char *cmd, char *path)
-{
-	int	i;
-
-	if (access(tmp, X_OK) == 0)
+	i = 0;
+	if (!s1 || !s2)
+		return (-1);
+	while (i < n && s1[i] && s2[i])
 	{
-		i = -1;
-		while (paths[++i])
-			free(paths[i]);
-		free(paths);
-		free(cmd);
-		free(path);
-		return (1);
+		if (s1[i] != s2[i])
+			return (s1[i] - s2[i]);
+		i++;
 	}
-	return (0);
-}
-
-int ft_strncmp(const char *s1, const char *s2, size_t n)
-{
-    size_t i;
-
-    i = 0;
-    if (!s1 || !s2)
-        return (-1);
-    while (i < n && s1[i] && s2[i])
-    {
-        if (s1[i] != s2[i])
-            return (s1[i] - s2[i]);
-        i++;
-    }
-    if (i == n)
-        return (0);
-    return (s1[i] - s2[i]);
-}
-
-int	open_file(char *argv, int i)
-{
-	int	file;
-
-	file = 0;
-	if (i == 0)
-		file = open(argv, O_WRONLY | O_CREAT | O_APPEND, 0444);
-	else if (i == 1)
-		file = open(argv, O_WRONLY | O_CREAT | O_TRUNC, 0444);
-	else if (i == 2)
-		file = open(argv, O_RDONLY, 0444);
-	if (file == -1)
-		exit(EXIT_FAILURE);
-	return (file);
+	if (i == n)
+		return (0);
+	return (s1[i] - s2[i]);
 }
